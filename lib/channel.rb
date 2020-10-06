2cf3d20ce8d598e17cd833b46bac4e5e282f8152
Ringo Alcock <ringo.alcock@gmail.com>
2020-10-06 16:08:18 -0700

    create channel class and define initialize and self.life methods

---
+++ lib/channel.rb
@@ -0,0 +1,33 @@
+require 'httparty'
+require 'dotenv'
+Dotenv.load
+
+require_relative 'recipient'
+
+class Channel < Recipient
+
+  attr_reader :name, :topic, :member_count
+
+  BASE_URL = "https://slack.com/api/conversations.list"
+
+  def initialize(slack_id, name, topic, member_count)
+    super(slack_id)
+    @name = name
+    @topic = topic
+    @member_count = member_count
+  end
+
+  def self.list
+    response = self.get(BASE_URL)
+    all_our_pretty_channels = []
+    response["channels"].each_with_index do |channel|
+      slack_id = channel["id"]
+      name = channel["name"]
+      topic = channel["purpose"]["value"]
+      member_count = channel["num_members"]
+      all_our_pretty_channels << Channel.new(slack_id, name, topic, member_count)
+    end
+    return all_our_pretty_channels
+  end
+
+end
