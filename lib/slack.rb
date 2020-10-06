2d084a150b550af88a6f02ba5b8f39ad03efd199
Jasmine <jasylop@gmail.com>
2020-10-05 19:05:47 -0700

    wrote a script to verify that the token works

--- lib/slack.rb
+++ lib/slack.rb
@@ -1,12 +1,26 @@
 #!/usr/bin/env ruby
+require "dotenv"
+require "httparty"
+require "pry"
+Dotenv.load
 
 def main
   puts "Welcome to the Ada Slack CLI!"
-  workspace = Workspace.new
+  #workspace = Workspace.new
 
-  # TODO project
+  # TODO projectdfd
 
   puts "Thank you for using the Ada Slack CLI"
 end
 
 main if __FILE__ == $PROGRAM_NAME
+
+URL = "https://slack.com/api/conversations.list"
+KEY = ENV["SLACK_TOKEN"]
+
+
+response = HTTParty.get(URL, query: { token: KEY } )
+
+binding.pry
+
+
